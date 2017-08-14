170470b70964c6609251d371f633a90589c81a68
megalord <jordan.griege@rackspace.com>
2017-08-14 17:09:26 -0500

    fix example

--- example/Main.purs
+++ example/Main.purs
@@ -5,18 +5,18 @@ import Control.Monad.Aff (Aff)
 import Control.Monad.Eff (Eff)
 import Data.Foldable (sequence_)
 
-import Neovim.Buffer (setLine)
+import Neovim.Buffer (setLines)
 import Neovim.Plugin (command, commandSync, defaultOpts, Args, Range, PLUGIN)
-import Neovim.Types (Vim)
-import Neovim.Vim (getCurrentBuffer)
+import Neovim.Types (Nvim)
+import Neovim.Nvim (getCurrentBuf)
 
 main :: forall e. Eff (plugin :: PLUGIN | e) Unit
 main = sequence_ [ commandSync "HelloPurescriptSync" defaultOpts helloCmdSync
                  , command "HelloPurescript" defaultOpts helloCmd
                  ]
 
-helloCmdSync :: forall e. Vim -> Args -> Range -> Aff (plugin :: PLUGIN | e) Unit
-helloCmdSync vim args range = getCurrentBuffer vim >>= \b -> setLine b 0 "Hi back! (sync)"
+helloCmdSync :: forall e. Nvim -> Args -> Range -> Aff (plugin :: PLUGIN | e) Unit
+helloCmdSync nvim args range = getCurrentBuf nvim >>= \b -> setLines b 0 1 true ["Hi back! (sync)"]
 
-helloCmd :: forall e. Vim -> Args -> Range -> Aff (plugin :: PLUGIN | e) Unit
-helloCmd vim args range = getCurrentBuffer vim >>= \b -> setLine b 0 "Hi back!"
+helloCmd :: forall e. Nvim -> Args -> Range -> Aff (plugin :: PLUGIN | e) Unit
+helloCmd nvim args range = getCurrentBuf nvim >>= \b -> setLines b 0 1 true ["Hi back!"]
